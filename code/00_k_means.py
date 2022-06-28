6f0f7450ada1e7548f8b60f7386cd14cb7c02d40
Scott Cardinal <archaeo_scott@yahoo.com>
2022-06-27 22:15:39 -0400

    k-means initialize code

--- code/00_k_means.py
+++ code/00_k_means.py
@@ -5,6 +5,8 @@ def diy_kmeans_initialize(X, k_clust):
 
     X_shuffled = np.random.shuffle(X)
 
+    # Choose k random points from the shuffled matrix
+
     k_centers = X_shuffled[np.random.choice(
         X_shuffled.shape[0], k_clust, replace=False), :]
 
