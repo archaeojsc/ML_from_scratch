22b03538b72d6653cb207d66c5287de972b40b00
J. Scott Cardinal <archaeo_scott@yahoo.com>
2022-06-29 15:22:02 -0400

    final kmeans algo

--- code/00_k_means.py
+++ code/00_k_means.py
@@ -9,9 +9,13 @@ import numpy as np
 
 def diy_kmeans_initialize(dat, k_clust):
 
-    # Shuffle the data matrix
+    # Copy the source data array
 
-    dat_shuffled = np.random.shuffle(dat)
+    dat_shuffled = dat.copy()
+
+    # Shuffle the copy
+
+    np.random.default_rng().shuffle(dat_shuffled, axis=0)
 
     # Choose k random points from the shuffled matrix
 
@@ -41,6 +45,8 @@ def diy_kmeans_distance(dat, centers):
 
 def diy_kmeans_assign(S):
 
+    # Label by column index of smallest value
+
     c = np.argmin(S, axis=1)
 
     return c
@@ -48,11 +54,74 @@ def diy_kmeans_assign(S):
 
 # %% Calculate new centers
 
+
 def diy_kmeans_update_centers(dat, k_labels):
+
+    # For each cluster label, find the mean of data points indexed by label
+    # position for that cluster
 
     new_k_centers = np.array([np.mean(dat[k_labels == k, :], axis=0)
                              for k in np.unique(k_labels)])
 
     return new_k_centers
+
+# %% Stopping criteria, cluster centers have not moved
+
+
+def diy_kmeans_converged(old_centers, new_centers):
+
+    # Returns a Boolean value of equivalence
+
+    converged = np.array_equiv(old_centers, new_centers)
+
+    return converged
+
+# %% DIY k-means algorithm
+
+
+def diy_kmeans(dat, n_clusters, max_iter=np.inf):
+
+    # Initial state and iteration counter
+    converged = False
+    i = 0
+
+    # Initialize centers
+    new_centers = diy_kmeans_initialize(dat, n_clusters)
+
+    # Repeat until convergence or maximum iterations reached
+
+    while (not converged) and (i <= max_iter):
+
+        # Update iteration counter
+        i += 1
+
+        # Store copy of prior centers
+        old_centers = new_centers.copy()
+
+        # Get distances to centers as matrix S
+        S = diy_kmeans_distance(dat, new_centers)
+
+        # Assign clusters to matrix C
+        C = diy_kmeans_assign(S)
+
+        # Calculate new centers
+        new_centers = diy_kmeans_update_centers(dat, C)
+
+        # Status update for "sanity checks" every 10 iterations
+        if i % 10 == 0:
+            print("Iteration:\t", i)
+
+        # Check stopping criteria
+        converged = diy_kmeans_converged(old_centers, new_centers)
+
+    # Convergence notification
+    print('Converged in %s iterations.' % i)
+
+    # Create dictionary of labelled cluster centers
+    centers = {k: new_centers[k, :] for k in np.unique(C)}
+    labels = C
+
+    return centers, labels
+
 
 # %%
